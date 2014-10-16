62c0dda3144f05204e73d226d829c22f8fa94835
GiacomoPinardi <pinobo1997@gmail.com>
2014-10-16 21:22:55 +0200

    version 0.1

---
+++ src/bookstat/BookReader.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author giacomo
+ */
+public class BookReader {
+    
+    int[] freq = null;
+    double[] perc = null;
+    int tot;
+    Scanner lettore = null;   
+        
+    public void read (String fileName) throws FileNotFoundException {
+        String s;
+        char c;
+        freq = new int[26];
+        perc = new double[26];
+        
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = 0;
+            perc[i] = 0;
+        }
+        tot = 0;
+        
+        File f = new File(fileName);
+        lettore = new Scanner (f);
+        
+        while (lettore.hasNextLine()) {
+            s = lettore.nextLine().toLowerCase();
+            for (int i = 0; i < s.length(); i++) {
+                c = s.charAt(i);
+                if ((c >= 'a') && (c <= 'z')) {
+                    freq[c -'a'] ++;
+                    tot ++;
+                }                
+            }
+        }
+        
+        if (tot != 0) {
+            for (int i = 0; i < perc.length; i++) {
+            perc[i] = (100d * freq[i])/tot;
+            }
+        }  
+        
+    }
+    
+    public double[] getPercentage () {
+        return this.perc;
+    }
+    
+    // DEBUG:
+    public void write () {
+        for (int i = 0; i < freq.length; i++) {
+            System.out.print(perc[i]);
+            System.out.print(" - ");
+            System.out.println(freq[i]);
+        }
+        System.out.println("Tot: " + tot);
+    }
+         
+}
---
+++ src/bookstat/BookStat.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstat;
+
+import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author giacomo
+ */
+public class BookStat extends Application {
+
+    public static void main(String[] args) {
+        
+        launch(args);
+        
+    }
+    
+    @Override
+    public void start(Stage stage) throws FileNotFoundException {
+        
+        String fileName = "WarAndPeace";
+        stage.setTitle(fileName + " Book Stat");
+        
+        Group root = new Group();                
+        Canvas canvas = new Canvas(640, 480);        
+        GraphicsContext gc = canvas.getGraphicsContext2D();        
+        Drawer d = new Drawer(gc, (canvas.getHeight()/100)*6);
+        //  
+        BookReader br = new BookReader();
+        
+        br.read("data/" + fileName.toLowerCase() + ".txt");
+        
+        double v[] = br.getPercentage();
+        //
+        d.drawAxis(Color.RED, 2);
+        d.drawBars(v);
+        
+        root.getChildren().add(canvas);
+        
+        stage.setScene(new Scene(root));
+        
+        stage.show();
+    }
+    
+}
